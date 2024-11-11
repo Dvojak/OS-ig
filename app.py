9e84f81e0ea552a473121e0f25dac543640ab9dc
Dvojak <118245733+Dvojak@users.noreply.github.com>
2024-11-11 09:15:46 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World! - nnova verzia"
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
